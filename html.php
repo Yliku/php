2bf4b7f74969dfa6f0de3120707678dc3ab7087e
Yliku <370495310@qq.com>
2018-03-15 18:55:53 +0800

    增加了html标签知识点

--- html.php
+++ html.php
@@ -1,36 +1,57 @@
-<html>
+<html>	
 <head>	<title>html基础</title>	 </head>		
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <!-- 用sublime写程序，输出到网页预览时，得在html中加入这句，不然浏览器无法识别会显示为乱码，
 	或者文件保存为utf-8 ，浏览器现在一般都会默认识别为utf-8 -->
 										<!-- HTML标签的注释,这注释符号真奇葩 -->
 <body bgcolor="#BEBEBE">					<!-- 设置背景颜色 -->
-<a>没链接的超链接标签</a>
-<br />									<!-- html中的换行符,最规范的写法,不要写成br -->
-<a href="https://baidu.com">(超链接跳转标签)跳转百度</a>		
-										<!-- 试试看不加https://的效果 href它是Hypertext Reference的缩写-->
-<p>段落1</p>
-<p>段落2</p>								<!-- 注意两个段落之间的距离 --> 
-<em>强调标签之em斜体-emphasize<br /></em>				<!-- emphasize 强调、着重 -->
-<strong>强调加重标签之strong粗体<br /></strong>		<!-- 比em更加强调 -->
-<q>引用标签之q引用-quote，给浏览器知道的，浏览器会自动加上双引号</q>
-<blockquote>引用标签之blockquote，给浏览器知道的，不会自动加上双引号，会分段且左右缩进</blockquote>
+<strong>sublime中输入!按Tab会自动补齐html的主体代码 <br /></strong>
+<a>a标签：超链接标签</a><br />				<!-- br /为html中的换行符,最规范的写法,不要写成br -->
+<a href="https://baidu.com">a标签跳转百度<br /></a>		<!-- 试试看不加https://的效果，或者换成www.   href是Hypertext Reference的缩写-->
+<q>q标签（quote）：短文本的引用；给浏览器知道的，该标签浏览器会识别为引用的内容，浏览器会自动加上双引号</q>
+<blockquote>blockquote标签：长文本的引用；该标签浏览器会识别为引用的内容，不会自动加上双引号，会分段且左右缩进</blockquote>
+<p>p标签：段落1</p>
+<p>p标签：段落2；注意两个段落之间的距离</p>								<!-- 注意两个段落之间的距离 --> 
+
+<ul>
+	<li>ul-li标签：无序列表；常用于无需排序的文章标题列表</li>
+	<li>1、前面会自动添加园点</li>
+	<li>2、另起一行，注意不是另起一段，段与段之间是有间隙的</li>
+	<li>3、li是list item，ol是ordered list，ul是unordered list无序列表</li>
+</ul>
+<ol>
+	<li>ol-li标签：有序列表；常用于文章阅读量排行榜</li>
+	<li>1、前面自动添加序号，默认序号从1开始</li>
+	<li>2、另起一行，<strong>留意ul和ol的区别</strong></li>
+</ol>
+
+<em>em标签（emphasize）：强调-斜体<br /></em>			<!-- emphasize 强调、着重 -->
+<strong>strong标签：加强版强调-粗体<br /></strong>		<!-- 比em更加强调 -->
+<address>address标签：地址标签，斜体且另起一行；该标签会让浏览器识别这是地址，地址；北京市丰台区xx大街xx号</address>
 <pre>pre标签：可以保留文本中的所有格式，包括多少个空格和回车，
 因为html里面无论同时输入了多少个空格和回车，都会被当成一个空格来显示，而在pre标签中会保留所有格式：
 5个空格：     。
 1个空格： 。
 如果不想用pre标签来输出空格，可以用& nbsp;来输出空格！注意分号！！nb(牛逼)sp(space) 好牛逼的空格!哈哈哈，其实是& nbsp;no-breaking space:不间断空格
 </pre>
-以下为hr(人事，其实是horizontal rule水平线)分割的空标签，和换行br标签一样，只有结束标签
+hr /标签（horizontal rule水平线）：水平分割线；空标签，和换行br标签一样，只有结束标签
 <hr />	
-address标签：
-<address>地址标签：北京市丰台区xx大街xx号，address标签会变斜体且另起一行</address>
-
+<div>div标签（divide）：把某个区域框起来，并另起一行，可通过css进行该区域样式的装饰，使每个区域呈现不同的效果</div>
+<div>div标签：就是一个框，将里面的东西框起来，然后通过css来使这个框按照所需要的样式显示出来</div>1234 <div>4567</div>
+<div>div标签形象的比喻：html=页面的家，div=页面的房间，css=对房间的装饰</div>
+span标签：<span>你看该标签把数据</span>连在一起了，不会和div标签一样另起一行；
+<div id="区别">	<!-- id为每个div板块添加名称 -->
+div和span区分：
+	<div>
+	两个都是用来划分区间但是没有实际语义的标签，相同之处就是都可以通过css来修饰其中的内容；<br />
+	差别就在于div是块级元素，不会其他元素在同一行；<span>而span是内联元素~</span>可以与其他元素位于同一行~<br />
+	</div>
+</div>
 
 <!-- img 是html中的单标签：单标签得加空格和斜杠!!!!	<br>换行<hr>水平线<img>插入图片<input>输入框<param>对象<meta>元信息<link>定义文档与外部资源的关系	 --> 	
 <img src="./basis/chicken1.jpg" />		<!-- 插入图片,本地图片放在根目录下的可以直接访问 --> 	
 										<!-- src就是 source（来源），这里是指图片地址 --> 				
-<img src="./basis/images/couple.jpg" />		<!-- 当图片位置和源代码的父文件夹位置在同一父文件夹时候 应先返回父级文件夹../ 因此src地址填"../images/baby.jpg" -->			
+<img src="./basis/images/couple.jpg" />	<!-- 当图片位置和源代码的父文件夹位置在同一父文件夹时候 应先返回父级文件夹../ 因此src地址填"../images/baby.jpg" -->			
 
 <form>								<!-- 插入表单单单单单单 --> 
 登录名:<input type="text" name="username">
@@ -54,7 +75,5 @@ echo  date("y-m-d H:i:s",time());	//得用双引号！！！大写的H表示输�
  ?>
 			
 </body>
-
-
 
 </html>
