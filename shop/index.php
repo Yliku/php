cbcb010d641807ea5b5a7305686dfe4519e8a496
Yliku <370495310@qq.com>
2018-04-26 04:07:14 +0800

    测试了vertical-align用法

--- shop/index.php
+++ shop/index.php
@@ -7,10 +7,9 @@
 		div{width:1000px;margin:0 auto;border:1px solid red;}
 		hr{width:1000px;margin:0 auto;}
 
-		.top1:after,.top2:after{content:"";	display:table;	clear:both; }
-		.top1 img{float:left;height: 10px;width:10px;}
-		.top1 span{float:right;}
+		.top1:after,.top2:after{content:"";	display:table;	clear:both; } 	/*清浮动*/
 		.top1 li{list-style:none;float:left;margin-right:10px;} 	 
+		.top1 span{float:right;}
 		/*行高line-height设置和盒子/图片高度一样，可以实现垂直居中   list-style-image: url(./images/li_bg.gif);*/
 		.top1 a:link{color:green;text-decoration:none;} 		/*取消字体修饰样式，在这里是清除a标签超链接的默认下划线*/
 		.top1 a:hover{color:red;text-decoration:underline;} 					/*滑过a标签，a标签改变颜色*/
@@ -38,8 +37,18 @@
 
 	<div class="top2">		<!-- top2开始 -->
 		<img src="images/logo.jpg" alt="慕课网" style="float:left;height: 50px;">
-		<span style="float:right;">1234</span>
-		<img src="images/tel.jpg" style="height: 20px;float:right;vertical-align:middle;text-align:center">
+		<div style="float:right;width:150px;margin:15px 0;">
+			<img src="images/tel.jpg" style="height: 20px;vertical-align:middle;">  	<!-- 为什么此处使用vertial-align会生效？？？？ -->
+			<span >0756-1234567</span>
+			<!--
+			 vertical-align:middle 垂直居中，只对行内元素有效，对块状元素无效！可用display:inline-block设置为行内块状元素 。
+			所谓inline-block行内块状元素，即可以与inline水平元素混排（在同一行），又能设置高宽属性的元素，
+			例如图片，按钮，单复选框，单行/多行文本框等HTML控件，只有这些元素默认情况下会对vertical-align属性起作用。
+			1、当父元素top2设置了line-height:200px;时，vertical-align:middle;将会根据父元素的高度在100px的位置垂直居中；
+			2、当父元素没有设置line-height时，可以试试直接设置height，将会看的更直观，
+			      这时vertical-align:middle;将会根据行内的兄弟元素对齐；在兄弟元素的中间水平线上对齐；
+			-->
+		</div>
 	</div>					<!-- top2结束 -->
 	<hr>
 	
