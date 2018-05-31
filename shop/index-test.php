53dc27055e157faeaaf5f87053e952c29b431f29
Yliku <370495310@qq.com>
2018-05-31 23:13:01 +0800

    将css样式分离出来

--- shop/index-test.php
+++ shop/index-test.php
@@ -1,101 +1,8 @@
 <html>
 <head>
 	<title>我的电商网站</title>
-	<style>
-		*{margin:0;padding:0;font-size:12px;font-family: "微软雅黑";} 		/*设置全局边框和字体*/
-		body{background: #F5F5F5;}
-		.top1,.top2,.content_top,.body,.btm{width:1000px;margin:0 auto;}
-
-		.top1:after,.top2:after,.content_top:after,.body:after{content:"";	display:table;	clear:both; } 	/*清浮动!!!!  	*/
-		.top{background:#c5d3e5;width:100%;border:none;height:30px;line-height: 30px;}
-		.top1 li{list-style:none;float:left;margin-right:5px;} 	 /*list-style:none没有样式/disc实心圆（默认）/circle空心圆/square实心方块*/
-		.top1 span{float:right;}
-		/*行高line-height设置和盒子/图片高度一样，可以实现垂直居中   list-style-image: url(./images/li_bg.gif);*/
-		.top1 a:link,.content_top li a:link{color:green;text-decoration:none;} 		/*取消字体修饰样式，在这里是清除a标签超链接的默认下划线*/
-		.top1 a:hover,.content_top li a:hover{color:red;text-decoration:underline;} 					/*滑过a标签，a标签改变颜色*/
-		/*
-		a:link 未访问时的状态；		a:visited 已访问过的状态；		a:hover 鼠标移动到链接上时的状态；
-		定义CSS时候的顺序不同，也会直接导致链接显示的效果不同。
-		原因可能在于浏览器解释CSS时遵循的“就近原则”。
-		浏览器判断a标签属于哪个状态，会取最近的样式进行解析，不会再重复向上/远重复解析。
-		正确的顺序（由上往下）：a:link、a:visited、a:hover、a:active
-		如果将a:hover放在最上面/远，那永远也不会得到hover的效果;
-
-		!!!不建议使用border来查看布局，建议使用background-color来看布局，因为border至少有1px，在很严谨的地方会严重影响到布局！
-		*/
-		.top2{background: white;}		
-		.tel{color:red;}
-
-		.content_top{border-radius:4px;background:gray;height:30px;} 	/*设置边框的圆角属性*/
-		/*border-top-left-radius:5px 10px;表示top-left这个角的水平圆角半径为5px，垂直圆角半径为10px。*/
-		.content_top li{list-style:none;width:100px;float:left;text-align:center;line-height: 30px;}
-		.content_top li a{font-size:16px;color:white;}
-		.content_top form{float:right;}
-		.search_text{
-			width:200px;height:24px;margin-top:3px;margin-right:20px;
-			background:url(./images/search.jpg) no-repeat right center; 	/*搜索框图片，不重复，居右，垂直居中*/
-			/*等同以下三句
-			background-image:url(./images/search.jpg);
-			background-repeat:no-repeat;
-			background-position:right center;
-			*/
-			background-color: white;
-			padding-right:25px; 	/*输入文字会遮盖搜索的背景图片，设置填充限制，填充大小等同于搜索图片宽度*/
-			border:1px solid white; /*设置边框线宽度，更加简洁好看*/
-		}
-		.body{margin:5px auto;}
-		.body1{height:400px;background-color:#A0D468;width:400px;float:left;border-radius:4px;}
-		.body2{height:400px;background-color:#AC92DC;width:180px;float:left;margin:0 10px;border-radius:4px;}
-		.body_title{height:30px;border-bottom:2px solid #E8E8E8;}
-		.body_title h2{width:70%;float:left;color:gray;line-height: 30px;padding-left:15px;}
-		.body_title a{width:20%;float:right;color:gray;height:30px;line-height: 30px;text-decoration: none;text-align:right;}
-		.body_title{clear:both;}
-
-		.pic_news{line-height: 20px;margin:10px;}
-		.pic_news img{
-			float:left;margin-right:10px;
-			cursor:pointer;		
-			/*pointer鼠标移到这里时变成一只伸出食指的手，和a标签的效果一样，只是点击了不能跳转，
-			其他参数有：default箭头，crosshair十字，progress箭头加沙漏等*/
-			transition:all 1s;	/*代表这个转换过程在1s内完成*/
-		}
-		.pic_news img:hover{
-			transform:rotate(360deg);	 /*旋转360度*/
-			/*
-			transform:scale(1.5); 	在鼠标放到图片上的时候图片按比例放大1.5倍
-			神奇的transform转换，其属性有：斜拉(skew)，缩放(scale)，旋转(rotate)以及位移(translate)。
-			*/
-		}
-
-		.news_list{margin: 10px;line-height: 20px;}
-		.news_list li{list-style:none;}
-		.news_list em{
-			background:url(./images/li-bg.jpg) no-repeat;
-			font-style:normal; /*em里的字体不倾斜*/		
-		}
-		.news_list_btm em{ 		/*为什么使用了上级.news_list em后，直接在em里面使用类选择器定义css样式，background-position不起作用？？？*/
-			background:url(./images/li-bg.jpg) no-repeat;
-			font-style:normal;
-			background-position:0px -16px;	
-			/*水平向右偏移0px;垂直向上偏移16px;不限制em宽度和设置no-repeat的话，背景图像是上下左右重复的,所以16px和-16px效果是一样的*/
-		}		
-		/*
-		.body_left,.body_middle,.body_right{width:330px;display:inline-block;}  
-		上面代码中的浮动如果改为行内块状元素，行内块状元素会出现默认间距，浮动元素则没有间距；
-		要留意浏览器内 行内块状元素 之间的默认间距，一般是标签之间的tab缩进造成的空格，360浏览器是3px
-		（注意：换行符、tab(制表符)、空格都会造成此问题）
-		*/
-
-		.transition
-
-
-
-		.btm{background:#c5d3e5;width:100%;height:30px;position:absolute;bottom:0;border:none;text-align:center;line-height: 30px;color:gray;}
-		/*    	背景颜色				宽度			高度			绝对定位			定位在最下面	没有边框		文字水平居中		行高等于div高度，垂直居中 	文字颜色为gray*/
-		.btm a{color:#1d5494;text-decoration:none;}
-		.btm a:hover{color:#1d5494;text-decoration:underline;}
-	</style>
 	<link rel="shortcut icon" href="./images/favicon.ico">
+	<link type="text/css" href="style/main.css" rel="stylesheet">
 </head>
 <body>
 	<div class="top">
@@ -143,7 +50,7 @@
 		<div class='body1'>
 			<div class='body_title'>
 				<h2>新闻列表</h2>
-				<a href="">More&gt;&gt;</a>
+				<a href="list.html">More&gt;&gt;</a>
 			</div>
 
 			<div class='pic_news'>
