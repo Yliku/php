fe2460aa1a670c32aeaab42d13b6f8ec669af993
Yliku <370495310@qq.com>
2018-08-31 08:41:26 +0800

    小修改

--- MVC/libs/db/mysql.class.php
+++ MVC/libs/db/mysql.class.php
@@ -4,26 +4,26 @@ class mysql{
 	function err($error){
 		echo '错误信息为：'.$error;
 	}
-
-	function connect($config){		//封装连接数据库	
-		//$config配置数组：array($dbhost,$dbuser,$dbpsw,$dbname,$dbcharset)
+	static $con = null;
+	static function connect($config){		//封装连接数据库	
+		//$config配置数组：$config = array($dbhost,$dbuser,$dbpsw,$dbname,$dbcharset)
 		extract($config);	//extract提取，提取$config关联数组，使用数组键名作为变量名，数组键值作为变量值。
-		if(	$con = mysqli_connect($dbhost,$dbuser,$dbpsw)	){
+		if(	self::$con = mysqli_connect($dbhost,$dbuser,$dbpsw)	){
 			echo '数据库连接成功。<br>';
+			if(	mysqli_select_db(self::$con,$dbname)	){
+				echo '选择子库成功。<br>';
+				mysqli_query(self::$con,'set names'.$dbcharset); 	//设	置字符集
+			}else{
+				echo '选择子库失败，原因：'.mysqli_error(self::$con);
+			}
 		}else{
 			echo '数据库连接失败，原因：'.mysqli_connect_error();
 		}		
-		if(	mysqli_select_db($con,$dbname)	){
-			echo '选择子库成功。<br>';
-		}else{
-			echo '选择子库失败，原因：'.mysqli_error();
-		}
-		mysqli_query($con,'set names'.$dbcharset); 	//设置字符集
 	}
 
 	function query($sql){			//封装执行sql命令
 		if(	!($query = mysqli_query($sql))	){
-			echo '执行sql出错，sql语句为：'$sql.'<br>原因为：'.mysqli_error();
+			echo '执行sql出错，sql语句为：'.$sql.'<br>原因为：'.mysqli_error();
 		}else{
 			return $query;
 		}
@@ -37,7 +37,7 @@ class mysql{
 		return isset($list)? $list : "";
 	}
 	function findOne($query){		//封装取单挑数据的命令
-		$rs = mysqli_fetch_array($query,MYSQL_ASSOC)
+		$rs = mysqli_fetch_array($query,MYSQL_ASSOC);
 		return $rs;
 	}
 
@@ -55,7 +55,8 @@ class mysql{
 
 	}
 }
-
+$config=array('dbhost'=>'127.0.0.1','dbuser'=>'root','dbpsw'=>'123','dbname'=>'test','dbcharset'=>'utf8mb4');
+mysql::connect($config);
 
 
  ?>
