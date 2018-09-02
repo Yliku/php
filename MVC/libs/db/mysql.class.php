e617c049283724573be27bb414a07710d2950e6f
Yliku <370495310@qq.com>
2018-09-03 01:59:00 +0800

    测试mysqli的封装方法，完善mysql insert的封装

--- MVC/libs/db/mysql.class.php
+++ MVC/libs/db/mysql.class.php
@@ -4,17 +4,17 @@ class mysql{
 	function err($error){
 		echo '错误信息为：'.$error;
 	}
-	static $con = null;
-	static function connect($config){		//封装连接数据库	
+	public $con = null;
+	function connect($config){		//封装连接数据库	
 		//$config配置数组：$config = array($dbhost,$dbuser,$dbpsw,$dbname,$dbcharset)
 		extract($config);	//extract提取，提取$config关联数组，使用数组键名作为变量名，数组键值作为变量值。
-		if(	self::$con = mysqli_connect($dbhost,$dbuser,$dbpsw)	){
+		if(	$this->con = mysqli_connect($dbhost,$dbuser,$dbpsw)	){
 			echo '数据库连接成功。<br>';
-			if(	mysqli_select_db(self::$con,$dbname)	){
+			if(	mysqli_select_db($this->con,$dbname)	){
 				echo '选择子库成功。<br>';
-				mysqli_query(self::$con,'set names'.$dbcharset); 	//设	置字符集
+				mysqli_query($this->con,'set names '.$dbcharset); 	//设	置字符集，踩坑：注意空格！！！
 			}else{
-				echo '选择子库失败，原因：'.mysqli_error(self::$con);
+				echo '选择子库失败，原因：'.mysqli_error($this->con);
 			}
 		}else{
 			echo '数据库连接失败，原因：'.mysqli_connect_error();
@@ -22,8 +22,8 @@ class mysql{
 	}
 
 	function query($sql){			//封装执行sql命令
-		if(	!($query = mysqli_query($sql))	){
-			echo '执行sql出错，sql语句为：'.$sql.'<br>原因为：'.mysqli_error();
+		if(	!($query = mysqli_query($this->con,$sql))	){
+			echo '执行sql出错，sql语句为：'.$sql.'<br>原因为：'.mysqli_error($this->con);
 		}else{
 			return $query;
 		}
@@ -49,14 +49,49 @@ class mysql{
 	function insert($table,$arr){
 		//$arr我们定为关联数组，因为当我们的字段很多时，用关联数组来书写就可以减少字段对错的失误
 		foreach($arr as $key=>$value){
-			$keyArr[]="`".$key."`";					//不用逗号隔开？？
-			$valueArr[]="'".$value."'";				//不用逗号隔开？？
+			$keyArr[]="`".$key."`";					//符号是键盘Esc下面的符号，不是单引号
+			$valueArr[]="'".$value."'";				//这个是单引号了
 		}
-
+/*  
+https://www.cnblogs.com/shijianchuzhenzhi/p/6193097.html
+键盘Esc下面的 ` 符号是 MySQL 的转义符，避免和 mysql 的本身的关键字冲突，
+只要你不在列名或表名中使用 mysql 的保留字或中文，就不需要转义。
+所有的数据库都有类似的设置，不过mysql用的是`而已。通常用来说明其中的内容是数据库名、表名、字段名，不是关键字。
+例如：
+select from from table;
+第一个from是字段名，最后的table表名，但是同时也是mysql关键字，这样执行的时候就会报错，
+所以应该使用select `from` from `table`;
+当然，为了便于阅读，不建议使用关键字作为字段名、表名，同时，应该对数据库名、表名、字段名用一对儿反引号包含。
+*/
+		$keys = implode(",", $keyArr);	//implode函数是把数组组合成字符串，用法： implode(分隔符，数组)
+		$values = implode(",", $valueArr);
+		$sql = "INSERT INTO ".$table."(".$keys.") VALUES(".$values.")";
+		$this->query($sql);
+		return mysqli_insert_id($this->con);
 	}
 }
-$config=array('dbhost'=>'127.0.0.1','dbuser'=>'root','dbpsw'=>'123','dbname'=>'test','dbcharset'=>'utf8mb4');
-mysql::connect($config);
 
+
+$mysql = new mysql();
+//数据库连接测试
+$config=array('dbhost'=>'127.0.0.1','dbuser'=>'root','dbpsw'=>'123','dbname'=>'test','dbcharset'=>'utf8');
+//踩坑：注意是utf8，不是utf-8，不然会乱码！！！
+$mysql->connect($config);
+
+//插入测试
+$sql_insert = 'INSERT INTO review(name) VALUES("晓黄")';
+$result1 = $mysql->query($sql_insert);
+var_dump($result1);
+
+//搜索测试
+$sql_search = 'SELECT * FROM review';
+$result2 = $mysql->query($sql_search);
+$list = $mysql->findAll($result2);
+var_dump($list);
+
+//插入封装的测试
+$arr=array('id'=>'5','name'=>'小红');	//注意id是主键，每次刷新得改下，避免重复报错
+$id = $mysql->insert('review',$arr);
+var_dump($id);
 
  ?>
