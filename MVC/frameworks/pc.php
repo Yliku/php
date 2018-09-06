a4ed85ba54a373b0ae93e942ad8e9e14769abae5
Yliku <370495310@qq.com>
2018-09-06 08:37:04 +0800

    测测试__FILE__

--- MVC/frameworks/pc.php
+++ MVC/frameworks/pc.php
@@ -1,10 +1,10 @@
 <?php 
 	//启动引擎文件
 	$currentdir = dirname(__FILE__);
-	include_once($currentdir.'/include.list.php');
-	foreach ($paths as $path) {
-		include_once($currentdir.'/'.$path);
-	}
-	
-	
+	// include_once($currentdir.'/include.list.php');
+	// foreach ($paths as $path) {
+	// 	include_once($currentdir.'/'.$path);
+	// }
+	var_dump($currentdir);
+	echo __FILE__;
  ?>
