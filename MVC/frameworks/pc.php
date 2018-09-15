0a8266b98f0bb9d67368582ac17d3f2f24a2dc2e
Yliku <370495310@qq.com>
2018-09-15 18:19:28 +0800

    完善入口文件

--- MVC/frameworks/pc.php
+++ MVC/frameworks/pc.php
@@ -1,10 +1,45 @@
 <?php 
 	//启动引擎文件
 	$currentdir = dirname(__FILE__);
-	// include_once($currentdir.'/include.list.php');
-	// foreach ($paths as $path) {
-	// 	include_once($currentdir.'/'.$path);
-	// }
-	var_dump($currentdir);
-	echo __FILE__;
+	include_once($currentdir.'/include.list.php');
+	foreach ($paths as $path) {
+		include_once($currentdir.'/'.$path);
+	}
+	// var_dump($currentdir);
+	// echo __FILE__;
+
+	class PC{
+		public static $controller;
+		public static $method;
+		private static $config;
+		/*
+		$config是二维关联数组，保存了数据库对应的配置，视图引擎对应的配置，	
+		举例： 
+		$config = array(	
+			'dbconfig'=>array('dbhost'=>'','dbuser'=>'','dbpsw'=>'','dbname'=>'','dbcharset'=>''),
+			'viewconfig'=>array()
+			)
+		*/
+		private static function init_db(){
+			DB::init('mysql',self:$config['dbconfig']);
+		}
+		private static function init_view(){
+			VIEW:init('Smarty',self::$config['viewconfig']);
+		}
+		private static function init_controller(){
+			self::$controller = isset($_GET['controller'])?daddslashes($_GET['controller']):'index';
+		}
+		private static function init_method(){
+			self::$method = isset($_GET['method'])?daddslasher($_GET['method']):'index';
+		}
+		public function run($config){
+			self::$config = $config;
+			self::init_db();
+			self::init_view();
+			self::init_controller();
+			self::init_method();
+			C(self::$controller,self::$method);
+		}
+	}
+
  ?>
