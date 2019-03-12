4680825029e14d0e77bd29f210f13b9dfe3795a9
Yliku <370495310@qq.com>
2019-03-12 15:08:13 +0800

    学习自动化脚本 .bat .sh，学习笔记在 ：我的笔记.nyf

---
+++ git-auto.sh
@@ -0,0 +1,10 @@
+@echo off
+echo first test
+rem 222
+echo 1111
+dir=`pwd`
+echo $dir
+d:/
+ls
+dir
+start ping baidu.com 
--- mysql/mysqli.php
+++ mysql/mysqli.php
@@ -3,7 +3,7 @@ $con = mysqli_connect($host,$name,$psw);		//建立数据库连接，传递地址
 mysqli_select_db($con,$name);					//选择需要操作的数据库（该用户的数据库）
 mysqli_query($con,"set names 'utf8'");			//设置当前连接使用的字符编码，一般使用utf8编码
 //数据库查询：
-$res=mysqli_query($con,'select * from user limit 1');	
+$res=mysqli_query($con,'SELECT * from user limit 1');	
 //建立连接后就可以进行查询，采用mysql_query加sql语句的形式向数据库发送查询指令
 //limit 1,2 表示从第1+1条开始，连续读2条
 $row = mysqli_fetch_array($res);	//查询类的语句会返回一个资源句柄/资源变量（resource），可以通过该资源变量获取查询结果集中的数据。
@@ -19,7 +19,7 @@ limit 后只有一个数字 例如limit a   是（从结果第一行开始）取
 $name = '李四';
 $age = 18;
 $class = '高三一班';
-$sql = "insert into user(name, age, class) values($name, $age, $class)";
+$sql = "INSERT into user(name, age, class) values($name, $age, $class)";
 mysqli_query($con,$sql);	//执行插入语句
 var_dump($sql);
 
