0badb4a70e0762360df31903ccb9b4d17f612066
Yliku <370495310@qq.com>
2019-05-21 16:56:55 +0800

    Laravel框架：event 结合 queue使用

--- laravel/app/Listeners/EventListener.php
+++ laravel/app/Listeners/EventListener.php
@@ -6,7 +6,8 @@ use App\Events\Event1;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class EventListener
+class EventListener implements ShouldQueue  
+//如果触发器执行的时间比较长，可以将触发器添加到queue队列来执行，只需使用 ShouldQueue 的接口即可
 {
     /**
      * Create the event listener.
