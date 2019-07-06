8ef45425f48591d7ac4ae29a9fa2742131973935
Yliku <370495310@qq.com>
2019-07-06 19:24:02 +0800

    修改注册代码，自定义返回注册返回内容

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -28,7 +30,8 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    // 登陆成功，重定向到 home路由
+    // protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -48,11 +51,18 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        $messages = [       //自定义错误信息
+            'email.unique' => '重复的邮箱',
+            'required' => '必填项',
+        ];
+
+        // make()传入参数进行验证，传递给 make 方法的第一个参数是需要验证的数据，第二个参数是要应用到数据上的验证规则。
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-        ]);
+            // 'captcha' => 'required',
+        ],$messages);       //插入错误信息的参数
     }
 
     /**
@@ -68,5 +78,26 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    public function register(Request $request)
+    {
+        // 调用控制器中的 validator方法对 传入的所有参数（name\email\password）进行验证，$request->all() 输出所有键值的数组
+        // $this->validator($request->all())->validate();
+        $res = $this->validator($request->all());
+        if ($res->fails()) {  //验证失败，直接返回
+            return $res->errors();
+            // return $res->errors()->first('email', '<p>:message</p>');
+        }
+
+        // 调用控制器中 create 方法创建用户
+        // 使用 $user 初始化 Redistered 对象
+        // 通过 event() 函数触发注册事件
+        event(new Registered($user = $this->create($request->all())));
+        // 获取 guard 实例，并通过 guard 登录创建的用户
+        $this->guard()->login($user);
+        // 根据 $user 注册信息进行页面跳转
+        return $this->registered($request, $user)
+                        ?: 'success';
     }
 }
